3d7f32323477330bb7e6da020d1fbc86b5306bb3
sowjanyakunder <sowjanya.r@v2solutions.com>
2015-08-19 13:18:56 +0530

    Modified versionchange program

--- version.rb
+++ version.rb
@@ -1,27 +1,28 @@
 class Version
-def initialize(section,version_cd,version_nm)
+  
+	  def initialize(section,version_cd,version_nm)
+		   @section=section
+		   @version_cd=version_cd
+		   @version_nm=version_nm
+	  end
 
-@section=section
-@version_cd=version_cd
-@version_nm=version_nm
+	  def versionchange()
+		   file_names = ['build1.gradle']
+		   file_names.each do |file_name|
+		     
+		     text = File.read(file_name)
+		     cnt = /(#{@section}.*\{(\s.*)(\s.*))/.match (text)
+		     str1 = $1
+		     puts $2
+		     puts $3
+		     puts str1
 
-end
+		     File.open('output.txt', 'w') do |out_file|
+		      out_file.print text.gsub(/#{$1}/,"#{@section}{\n\t    versionName = \"#{@version_nm}\"\n \t    versioncode = #{@version_cd}")
+		     end
+		   end
+	   end
 
-def versionchange()
-file_names = ['build1.gradle']
-file_names.each do |file_name|
-text = File.read(file_name)
- 
-cnt = /(#{@section}.*\{(\s.*)(\s.*))/.match (text)
-str1 = $1
-puts $2
-puts $3
-puts str1
-File.open('output.txt', 'w') do |out_file|
-out_file.print text.gsub(/#{$1}/,"#{@section}{\n\t    versionName = \"#{@version_nm}\"\n \t    versioncode = #{@version_cd}")
-end
-end
-end
 end
 
 v = Version.new("QA",412,"NEWAND")
